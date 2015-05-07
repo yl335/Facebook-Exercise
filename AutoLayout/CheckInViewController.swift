f76640a576d3687c00a3b5f63002f45fa86131eb
Sara Lin <saralin@dropbox.com>
2015-05-06 23:22:56 -0700

    Upload class exercise

---
+++ AutoLayout/CheckInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CheckInViewController.swift
+//  AutoLayout
+//
+//  Created by Sara Lin on 5/6/15.
+//  Copyright (c) 2015 Sara Lin. All rights reserved.
+//
+
+import UIKit
+
+class CheckInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func OnCancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
